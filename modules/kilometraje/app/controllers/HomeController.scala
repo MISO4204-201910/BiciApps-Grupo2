09a13cfc4cba81735424749864de7f29ff9a94e6
David Ortiz <davideduardo.ortiz@gmail.com>
2019-04-18 00:03:20 -0500

    multi componentes listo

--- modules/kilometraje/app/controllers/HomeController.scala
+++ modules/kilometraje/app/controllers/HomeController.scala
@@ -2,10 +2,26 @@ package controllers.kilometraje
 
 import play.api.mvc._
 import javax.inject.Inject
+import play.api.Configuration
 
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(
+  val controllerComponents: ControllerComponents,
+  config: Configuration
+  ) extends BaseController {
+
+  val isModuleOn = config.get[Boolean]("gamification.kilometraje")
+
+  def validateModuleRoute(status: Result)(implicit request: Request[_]) = {
+    if (isModuleOn) {
+      status
+    } else {
+      NotFound
+    }
+  }
 
   def index = Action { implicit request =>
-    Ok("kilometraje")
+    validateModuleRoute(
+      Ok("kilometraje")
+    )
   }
 }
--- modules/kilometraje/build.sbt
+++ modules/kilometraje/build.sbt
@@ -5,3 +5,5 @@ libraryDependencies ++= Seq(
   jdbc,
   "com.h2database" % "h2" % "1.4.197"
 )
+
+PlayKeys.devSettings += ("play.http.router", "kilometraje.Routes")
